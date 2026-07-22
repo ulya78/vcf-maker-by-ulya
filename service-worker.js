99434b84f9e310a1fce4e356f427ee0572fc1d76
ulya78 <ulyazahrotusholihah078@gmail.com>
2026-07-22 09:14:11 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = "vcf-maker-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
